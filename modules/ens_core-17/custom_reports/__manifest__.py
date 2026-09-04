ded68acd18babc54ce92c114cfb24fdf93c1c1ee
BAHOU <dg.enswork@gmail.com>
2026-09-04 15:36:59 +0000

    [custom_reports] FEAT: Ajout colonne PU HT au bon de livraison DB
    Refs: #20

--- modules/ens_core-17/custom_reports/__manifest__.py
+++ modules/ens_core-17/custom_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {  # noqa: B018
     "name": "Enswork Reports",
-    "version": "17.0.1.44",
+    "version": "17.0.1.45",
     "category": "Sales",
     "summary": "Custom report templates by Enswork",
     "author": "ENSWORK",
